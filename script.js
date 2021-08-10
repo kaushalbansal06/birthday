d3e87d183b2ffec16b1957be8ebecfee1ef34ab0
kaushalbansal06 <60375971+kaushalbansal06@users.noreply.github.com>
2021-08-10 16:29:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a=="chinki" && b=="mausi")
+  	if(a=="chinky" && b=="mausi")
   	{
   	    window.location.href="start.html";
   	}
